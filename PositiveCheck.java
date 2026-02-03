42dd1188440304c6905d15310048f509ebc073b0
ssganga300-byte <ssganga300@gmail.com>
2026-02-03 10:57:20 +0530

    Add files via upload

---
+++ PositiveCheck.java
@@ -0,0 +1,13 @@
+public class PositiveCheck {
+    public static void main(String[] args) {
+        int num = 5;   
+
+        if (num > 0) {
+            System.out.println("The number is Positive");
+        } else if (num == 0) {
+            System.out.println("The number is Zero");
+        } else {
+            System.out.println("The number is Negative");
+        }
+    }
+}
