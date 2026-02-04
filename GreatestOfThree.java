192cff44c1cf1b1b2038ca335a227b48c4131a7a
ssganga300-byte <ssganga300@gmail.com>
2026-02-04 21:49:49 +0530

    Add files via upload

---
+++ GreatestOfThree.java
@@ -0,0 +1,27 @@
+javaimport java.util.Scanner;
+
+public class GreatestOfThree {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter first number: ");
+        int a = sc.nextInt();
+
+        System.out.print("Enter second number: ");
+        int b = sc.nextInt();
+
+        System.out.print("Enter third number: ");
+        int c = sc.nextInt();
+
+        if (a >= b && a >= c) {
+            System.out.println("Greatest number is: " + a);
+        } else if (b >= a && b >= c) {
+            System.out.println("Greatest number is: " + b);
+        } else {
+            System.out.println("Greatest number is: " + c);
+        }
+
+        sc.close();
+    }
+}
